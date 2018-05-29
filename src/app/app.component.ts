b522e9e00c6958adf78e6974114a30467df0f0be
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-29 16:05:38 -0300

    Instalando o json-server para testes com requisição

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  cidades = [
+    { id: 1, nome: 'Uberlândia' },
+    { id: 2, nome: 'São Paulo' },
+    { id: 3, nome: 'Florianópolis' },
+    { id: 4, nome: 'Curitiba' }
+  ];
+
+  adicionar(nome: string) {
+    alert(nome);
+  }
+
+  excluir(id: number) {
+    alert(id);
+  }
+
+  atualizar(cidade: any) {
+    alert(JSON.stringify(cidade));
+  }
 }
